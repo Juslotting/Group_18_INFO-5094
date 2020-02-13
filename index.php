587aac4bd0e468b527a1cc873c2edec06aa41ad0
Juslotting <57736545+Juslotting@users.noreply.github.com>
2020-02-13 18:58:56 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  *
  * Course 5094 LAMP 2
  * Professor Tom Hall
- * 
+ *
  * Group 18
  * Group Members / Authors
  * Andi Ausrotas
@@ -13,62 +13,112 @@
  * Justin Lott
  * Jadrienne Lovegrove
  * Nicholas Glover
- * 
+ *
  * File Information
  * File Name: Index.php
- * 
+ *
  * Purpose:
  * Main file for project, connects to database, contains php/html and displays upload page.
  */
 
-//Commented out because mysqli isn't working
-
-/*$con = new mysqli_connect("localhost","lamp2user","info5094","paths");
-if (mysqli_connect_errno()) {
-    echo "Unable to connect to MySQL! ". mysqli_connect_error();
-    exit();
-}*/
 session_start();
 
 if (isset($_POST['save'])) {
-    $target_dir = "/uploads";
-    //$f is the opened and read file
-    $f = fopen($_POST["file"]["tmp_name"], "r");
-    //target file is the file, to move, use tmp_name instead of name
-    $target_file = $_FILES["file"]["name"];
-    //filetype is the extension (.csv)
-    $fileType = pathinfo($target_file,PATHINFO_EXTENSION);
-    //checks if file is a csv
-    if ($fileType == "csv") {
-        if(move_uploaded_file($_FILES["file"]["tmp_name"])) {
-            $csv = fgetcsv($f, 1000);
-            echo $csv;
-            //$csv should be the csv file with each line read into an array
-            //here is where I was going to put each line into the database
+    if(isset($_FILES["file"])) {
+      if ($_FILES["file"]["error"] > 0) {
+            echo $_FILES["file"]["error"] . "<br />";
 
+        }
+      else {
+      echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+      echo "Type: " . $_FILES["file"]["type"] . "<br />";
+      echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+      echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
+      //filetype is the extension (.csv)
+      $fileType = pathinfo($_FILES["file"]["name"],PATHINFO_EXTENSION);
+      $targetdir = "uploads/";
 
+      //checks if file is a csv
+      if ($fileType == "csv") {
+          $storagename = "file.csv";
+          if(move_uploaded_file($_FILES["file"]["tmp_name"], "uploads/" . $storagename)) {
+          echo "Stored in: " . "uploads/" . $_FILES["file"]["name"] . "<br />";
+          $f = fopen($targetdir.$storagename, "r");
+          $csv = fgetcsv($f, 1000);
+          print_r($csv);
 
+          $con = mysqli_connect("localhost","lamp2user","info5094","paths");
+          if (mysqli_connect_errno()) {
+              echo "Unable to connect to MySQL! ". mysqli_connect_error();
+              exit();
+          }
 
-            $filename = basename($_FILES["file"]["name"]);
-            $location = "/uploads" . $filename;
-            //Commented out because mysql isn't working
-           /* $sqli = "INSERT INTO files (f_filename, f_location) VALUES ('{$filename}','{$location}')";
+          $path_name = $csv[0];
+          $operating_frequency = $csv[1];
+          $pi_description = $csv[2];
+          $pi_note = $csv[3];
+
+          $sqli = "INSERT INTO path_info (path_name,operating_frequency,pi_description,pi_note) VALUES ('{$path_name}','{$operating_frequency}','{$pi_description}','{$pi_note}')";
+          $result = mysqli_query($con,$sqli);
+          if ($result) {
+              echo "File has been uploaded";
+          };
+
+          $pb_distance = $csv[4];
+          $pb_ground_height = $csv[5];
+          $pb_antenna = $csv[6];
+          $pb_cable_type = $csv[7];
+          $pb_cable_length = $csv[8];
+
+          $sqli = "INSERT INTO path_beginning (pb_distance,pb_ground_height,pb_antenna,pb_cable_type,pb_cable_length) VALUES ('{$pb_distance}','{$pb_ground_height}','{$pb_antenna}','{$pb_cable_type}','{$pb_cable_length}')";
+          $result = mysqli_query($con,$sqli);
+          if ($result) {
+              echo "File has been uploaded";
+          };
+
+          $pe_distance = $csv[9];
+          $pe_ground_height = $csv[10];
+          $pe_antenna = $csv[11];
+          $pe_cable_type = $csv[12];
+          $pe_cable_length = $csv[13];
+
+          $sqli = "INSERT INTO path_ending (pe_distance,pe_ground_height,pe_antenna,pe_cable_type,pe_cable_length) VALUES ('{$pe_distance}','{$pe_ground_height}','{$pe_antenna}','{$pe_cable_type}','{$pe_cable_length}')";
+          $result = mysqli_query($con,$sqli);
+          if ($result) {
+              echo "File has been uploaded";
+          };
+          $loop = count($csv) - 13;
+          for ($i = 14; $i < $loop; $i++) {
+            $md_distance = $csv[i];
+            $i++;
+            $md_ground_height = $csv[i];
+            $i++;
+            $md_terrain = $csv[i];
+            $i++;
+            $md_obstr_height = $csv[i];
+            $i++;
+            $md_obstr_type = $csv[i];
+            $sqli = "INSERT INTO mid_points (md_distance,md_ground_height,md_terrain,md_obstr_height,md_obstr_type) VALUES ('{$md_distance}','{$md_ground_height}','{$md_terrain}','{$md_obstr_height}','{$md_obstr_type}')";
             $result = mysqli_query($con,$sqli);
             if ($result) {
                 echo "File has been uploaded";
-            };*/
+            };
+          }
+          }
+          else {
+            echo "File could not be uploaded";
+          }
         }
-        else {
-            echo "File failed to upload";
-        }
-    }
-    else {
-        echo "File not supported";
+      else {
+        echo "File was wrong type";
+      }
+      }
     }
 }
 ?>
 
 <h1>Upload and Download</h1>
+<p>Select csv file below, and then click Upload</p>
 <form class="form" method="post" action="" enctype="multipart/form-data">
 <label class="label">File:</label>
 <input type="file" name="file" class="input"> <br/>
